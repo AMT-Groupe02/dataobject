d1593fb414e5970ecba797fc7d796cfa83e0ff86
Luca Coduri <coduri.vitor@gmail.com>
2022-10-18 14:58:57 +0200

    init

---
+++ src/main/java/ch/amt/dataobject/App.java
@@ -0,0 +1,13 @@
+package ch.amt.dataobject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
